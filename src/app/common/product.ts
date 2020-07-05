e64363bc665cbbf1265640963433b99af50f9579
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2020-07-05 21:22:50 +0200

    Revert "delete old services"
    This reverts commit 72b97fac9c094e4ab1252f9d6703273258669f9d.

---
+++ src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    id: String;
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdate: Date;
+}
